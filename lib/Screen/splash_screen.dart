aebcac112b866f9379dc81126cdebce85b0e4102
Cinira Urani <marcus.urani20@gmail.com>
2024-12-04 11:48:30 -0300

    Corrigida estilização dos menus dropdown

--- lib/Screen/splash_screen.dart
+++ lib/Screen/splash_screen.dart
@@ -18,8 +18,8 @@ class QuizSplashScreen extends StatelessWidget {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
-              Color.fromARGB(255, 141, 141, 143),
-              Color.fromARGB(255, 122, 123, 131),
+              blue,
+              darkBlue,
             ],
           ),
         ),
@@ -39,9 +39,9 @@ class QuizSplashScreen extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 20),
-              Center(child: const DropdownMenuDifficulty()),
+              const Center(child: const DropdownMenuDifficulty()),
               const SizedBox(height: 20),
-              Center(child: const DropdownMenuCategories()),
+              const Center(child: const DropdownMenuCategories()),
               const Spacer(),
               Align(
                 alignment: Alignment.center,
